71c9080098546b4ec8b1c0d6e684de6631a102aa
usermuser <xakacb4@yandex.ru>
2015-04-16 15:04:43 -0400

    finished japp, but design is ugly

--- japp/urls.py
+++ japp/urls.py
@@ -1,8 +1,15 @@
 # coding: utf-8
 from django.conf.urls import patterns, include, url
-from .views import PdaChangesListView, PdaChangesDetailView
+from .views import PdaChangesListView, PdaChangesDetailView, PdaChangesRegisterView, PdaChangesCreateView
+from django.core.urlresolvers import reverse_lazy
 
 urlpatterns = patterns('',
-    url(r'^$', PdaChangesListView.as_view(), name='index'),
-    url(r'^(?P<pk>[0-9]+)/$', PdaChangesDetailView.as_view(), name='detail'),
+    url(r'^$', PdaChangesListView.as_view(), name='japp_index'),
+    url(r'^(?P<pk>[0-9]+)/$', PdaChangesDetailView.as_view(), name='japp_detail'),
+    url(r'^login/$', 'django.contrib.auth.views.login',
+          {"template_name" : "japp_login.html"}, name="japp_login"),
+    url(r'^logout/$', 'django.contrib.auth.views.logout',
+          {"next_page" : reverse_lazy('japp_login')}, name="logout"),
+    url(r'^register/$', PdaChangesRegisterView.as_view(), name='japp_register'),
+    url(r'^add/$', PdaChangesCreateView.as_view(), name='japp_add'),
 )
--- japp/views.py
+++ japp/views.py
@@ -2,6 +2,10 @@
 
 # Create your views here.
 
+from django.views.generic import CreateView 
+from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+
 from django.views.generic import ListView, DetailView
 from .models import Pda
 
@@ -12,3 +16,20 @@ class PdaChangesListView(ListView):
 class PdaChangesDetailView(DetailView):
     model = Pda
     template_name = ('japp_detail.html')
+
+
+class PdaChangesRegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'japp_register.html'
+    success_url = reverse_lazy('japp_index')
+
+class PdaChangesCreateView(CreateView):
+    model = Pda
+    template_name = 'japp_add.html'
+    fields = ['text','cpu']
+    success_url = reverse_lazy('japp_index')
+
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(PdaChangesCreateView, self).form_valid(form)
